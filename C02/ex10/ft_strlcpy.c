b8de5ef1fe31b91a027580723442e919c7d5283d
cabraham <cabraham@c1r7p1.42lehavre.fr>
2024-08-12 07:34:18 +0200

    Hello

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cabraham <cabraham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 07:05:28 by cabraham          #+#    #+#             */
+/*   Updated: 2024/08/12 07:16:57 by cabraham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (size == 0)
+	{
+		return (ft_strlen(src));
+	}
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < size)
+	{
+		dest[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
