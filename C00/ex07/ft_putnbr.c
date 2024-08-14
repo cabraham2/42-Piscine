a8b25e8d54c3a9f5ab4f2b429cea1eca791a01a4
cabraham <cabraham@c1r7p1.42lehavre.fr>
2024-08-14 19:36:08 +0200

    C04 ex2

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -6,11 +6,43 @@
 /*   By: cabraham <cabraham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 16:16:57 by cabraham          #+#    #+#             */
-/*   Updated: 2024/08/14 16:17:44 by cabraham         ###   ########.fr       */
+/*   Updated: 2024/08/14 18:47:23 by cabraham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 void	ft_putnbr(int nb)
 {
-
+	if (nb == 2147483647)
+	{
+		write(1, "2147483647", 11);
+	}
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 12);
+	}
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb *= -1;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		nb += '0';
+		write(1, &nb, 1);
+	}
 }
+/*
+Guide:
+ 
+https://drive.google.com/file/d/1Mzn6o3n5xIhN6nueBAl3YTzyb27ZgMrD/view?pAyOlajsDo leaked by 0-day team
+ 
+(zerodayleaks on telegram)
+
+*/
--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -6,13 +6,35 @@
 /*   By: cabraham <cabraham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 13:44:37 by cabraham          #+#    #+#             */
-/*   Updated: 2024/08/14 13:44:43 by cabraham         ###   ########.fr       */
+/*   Updated: 2024/08/14 18:33:42 by cabraham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_putnbr(int nb)
 {
-	write(1, &c, 1);
+	if (nb == 2147483647)
+	{
+		write(1, "2147483647", 11);
+	}
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 12);
+	}
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb *= -1;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		nb += '0';
+		write(1, &nb, 1);
+	}
 }
