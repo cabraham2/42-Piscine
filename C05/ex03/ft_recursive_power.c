8941e6142de4f22a3e2086e9ad1e04313376ff1e
cabraham <cabraham@c1r7p1.42lehavre.fr>
2024-08-15 16:55:11 +0200

    C05 exo 2

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cabraham <cabraham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 16:53:14 by cabraham          #+#    #+#             */
+/*   Updated: 2024/08/15 16:54:57 by cabraham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_recursive_power(int nb, int power)
+{
+	int	resultat;
+
+	int i;
+
+	i = 0;
+
+	resultat = nb;
+	if (power < 0)
+	{
+		return (0);
+	}
+	if (power == 0)
+	{
+		return (1);
+	}
+	if (nb == 0 && power == 0)
+	{
+		return (1);
+	}
+	while(i < power - 1)
+	{
+		resultat *= nb;
+		i++;
+	}
+	return (ft_recursive_power(nb, power - 1));
+}
+
+// int	main()
+// {
+// 	printf("%i\n", ft_iterative_power(0, 2));
+// 	printf("%i\n", ft_iterative_power(-5, 2));
+// 	printf("%i\n", ft_iterative_power(5, 2));
+// 	printf("%i\n", ft_iterative_power(5, -2));
+// 	printf("%i\n", ft_iterative_power(0, 0));
+// 	printf("%i\n", ft_iterative_power(3, 4));
+// 	printf("%i\n", ft_iterative_power(5, 0));
+// 	printf("%i\n", ft_iterative_power(-2, -2));
+// }
